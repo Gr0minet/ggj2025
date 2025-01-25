e2a0bd3fdd55bb2944e14688ee81c8c399fe942a
m09 <142691+m09@users.noreply.github.com>
2025-01-25 12:39:53 +0100

    Wip menu
    Signed-off-by: m09 <142691+m09@users.noreply.github.com>

--- godot/menu/player_join.gd
+++ godot/menu/player_join.gd
@@ -5,19 +5,18 @@ extends PanelContainer
 @onready var rich_text_label = $VBoxContainer/RichTextLabel
 
 func _ready() -> void:
-	player_reset(self.player_id)
+	player_reset()
 
 
-
-
-func player_reset(player_id: int) -> void:
+func player_reset() -> void:
 	rich_text_label.text = "[center]Joueur " + str(player_id + 1) + "
 [img]res://assets/xbox_button_color_a_outline.png[/img]
 pour rejoindre[/center]"
 
 
-func player_join(player_id: int) -> void:
-	rich_text_label.text = "[center]Joueur " + str(player_id + 1) + "
+func player_join(join_player_id: int) -> void:
+	if join_player_id == player_id:
+		rich_text_label.text = "[center]Joueur " + str(player_id + 1) + "
 Maintiens
 [img]res://assets/xbox_button_color_a_outline.png[/img]
 pour lancer[/center]"
@@ -28,6 +27,6 @@ func _unhandled_input(event: InputEvent) -> void:
 		if event.pressed and event.keycode == KEY_A:
 			player_join(self.player_id)
 		if event.pressed and event.keycode == KEY_Z:
-			player_reset(self.player_id)
+			player_reset()
 	if event.is_action_pressed("join_game"):
 		player_join(self.player_id)

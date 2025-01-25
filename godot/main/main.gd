03ebd7f524bb1b7081c581153df0957867ad79b6
Gr0minet <not@existing.abcdef>
2025-01-25 10:18:45 +0100

    Ajout baignoire

--- godot/main/main.gd
+++ godot/main/main.gd
@@ -6,3 +6,17 @@ extends Node3D
 
 func _ready() -> void:
 	_hud.start_timer(3)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("full_screen"):
+		_swap_fullscreen_mode()
+	elif event.is_action_pressed("exit"):
+		get_tree().quit()
+
+
+func _swap_fullscreen_mode():
+	if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)

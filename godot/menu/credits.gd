f491d7acb118e1b2d8d22c37cc6f05600acca27c
m09 <142691+m09@users.noreply.github.com>
2025-01-25 21:07:39 +0100

    Tweak credits
    Signed-off-by: m09 <142691+m09@users.noreply.github.com>

--- godot/menu/credits.gd
+++ godot/menu/credits.gd
@@ -9,4 +9,4 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_released("exit"):
-		queue_free()
+		hide()

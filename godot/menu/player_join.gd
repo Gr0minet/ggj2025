92d9b2aec7d9438d4ed76a6d8029ba21d9bd8c97
m09 <142691+m09@users.noreply.github.com>
2025-01-25 14:14:35 +0100

    Sync wip menu
    Signed-off-by: m09 <142691+m09@users.noreply.github.com>

--- godot/menu/player_join.gd
+++ godot/menu/player_join.gd
@@ -3,6 +3,11 @@ extends PanelContainer
 
 @export var player_id: int
 @onready var rich_text_label = $VBoxContainer/RichTextLabel
+@onready var progress_bar = $TextureProgressBar
+
+var player_joined: bool
+var join_touch_pressed = false
+const JOIN_DELAY = 1.5
 
 func _ready() -> void:
 	player_reset()
@@ -12,6 +17,8 @@ func player_reset() -> void:
 	rich_text_label.text = "[center]Joueur " + str(player_id + 1) + "
 [img]res://assets/xbox_button_color_a_outline.png[/img]
 pour rejoindre[/center]"
+	player_joined = false
+	progress_bar.value = 0.0
 
 
 func player_join(join_player_id: int) -> void:
@@ -20,13 +27,19 @@ func player_join(join_player_id: int) -> void:
 Maintiens
 [img]res://assets/xbox_button_color_a_outline.png[/img]
 pour lancer[/center]"
+	player_joined = true
+
+
+func _process(delta: float) -> void:
+	if join_touch_pressed:
+		progress_bar.set_value_no_signal(progress_bar.value + delta / JOIN_DELAY * 100)
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventKey:
-		if event.pressed and event.keycode == KEY_A:
+	if event.is_action_pressed("join_game") and event.device == 0:
+		if not player_joined:
 			player_join(self.player_id)
-		if event.pressed and event.keycode == KEY_Z:
-			player_reset()
-	if event.is_action_pressed("join_game"):
-		player_join(self.player_id)
+		else:
+			join_touch_pressed = true
+	if event.is_action_released("join_game"):
+		join_touch_pressed = false

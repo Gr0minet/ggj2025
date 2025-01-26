748f6f66b5a6275607b9e60647c3fcc44a4e6792
tehelka-gamedev <17590418+tehelka-gamedev@users.noreply.github.com>
2025-01-26 10:30:13 +0100

    add debug sphere to show spawn points

--- godot/bathtub_level/player_spawns.gd
+++ godot/bathtub_level/player_spawns.gd
@@ -1,10 +1,20 @@
+@tool
 class_name PlayerSpawns
 extends Node3D
+
+@export_category("Debug in editor")
+@export var show_spawn_points:bool = false :
+	set(_value):
+		show_spawn_points = _value
+		for c in get_children():
+			c.visible = _value
 
 var _spawn_available:Array[Vector3] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if not Engine.is_editor_hint():
+		show_spawn_points = false
 	reset_spawns()
 
 func reset_spawns() -> void:
@@ -23,4 +33,3 @@ func get_and_lock_spawn() -> Vector3:
 		return Vector3(-1, -1, -1)
 	
 	return spawn_pos
-	
